81baec121226dd8e44a8c6dee8bcc3fe1a05b59b
zengzoe <563282341@qq.com>
2018-04-17 19:52:28 +0800

    Initial commit
    upload code

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var CleanWebpackPlugin = require('clean-webpack-plugin'),
+    webpack = require('webpack'),
+    ExtractTextWebpackPlugin = require('extract-text-webpack-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var minify = process.env.ENV == 'prod'; //是否压缩
+
+module.exports = {
+    entry : {
+        index : './src/js/index.js'
+    },
+    output : {
+        path : __dirname + '/dist',
+        filename : 'js/[name].js'
+    },
+    module : {
+        rules : [{
+            test : /\.(css|scss)$/,
+            use : ExtractTextWebpackPlugin.extract({
+                use : [
+                    'css-loader?-url&-reduceTransforms',
+                    'sass-loader',
+                    'postcss-loader'
+                ]
+            })
+        },{
+            test : /\.js$/,
+            exclude : /node_modules/,
+            loader : 'babel-loader'
+        }]
+    },
+    plugins : [
+        new CleanWebpackPlugin('./dist'),
+        new ExtractTextWebpackPlugin('css/[name].css'),
+        new webpack.ProvidePlugin({
+            $ : '../lib/zepto.js'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from : './src/img',
+                to : 'img',
+                ignore : ['.gitkeep']
+            }
+        ]),
+        new HtmlWebpackPlugin({
+            inject : false,
+            minify : minify ? {
+                collapseWhitesspace : true,
+                minifyCSS : true,
+                minifyJS : true ,
+                removeComments : true
+            } : false,
+            template : './src/index.html'
+        })
+    ],
+    devtool: 'source-map'
+}
+
+
+
